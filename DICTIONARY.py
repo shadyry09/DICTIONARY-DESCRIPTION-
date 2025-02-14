05a6ba3e52705db3bb2b1b01cf4f72ab4aafab70
unbreakable.ry <rjaypenafiel1@gmail.com>
2025-02-14 16:30:32 +0800

    Add files via upload

---
+++ DICTIONARY.py
@@ -0,0 +1,37 @@
+DATA = {
+        "abandon": "to leave or to give up",
+        "abbreviate": "to shorten words, names, etc.",
+        "benefit": "profit/advantage",
+        "buffalo":"large wild ox or cow",
+        "chitchat":"gossip/talk",
+        "cytology": "study of cells",
+        "dibble":"tool used to make holes in t'he ground for planting",
+        "ditty":"little song/simple song",
+        "enigma":"mystery/puzzle",
+        "flagon":"large round container for liquids",
+        "gait":"manner of walking",
+        "heliport":"place where helicopters land and take off",
+        "incite":"to encourage",
+        "joggle":"to move rapidly/nervously",
+        "knoll":"small hill",
+        "leash":"strap/cord to keep a dog in control",
+        "mallet":"large wooden hammer",
+        "nape":"back of the neck",
+        "odontology":"study of teeth",
+        "pitfall":"trap/danger",
+        "quilt":"padded cover for a bed",
+        "replete":"full and satisfied'",
+        "sheen":"brilliant shining surface",
+        "twig":"little branch",
+        "undermine":"to weaken",
+        "vial":"small glass bottle",
+        "whorl":"coiled/spiral shape",
+        "xenophobia":"hatred, dislike, or fear of foreigners",
+        "yolk":"yellow part of an egg",
+        "zoology":"study of animals"
+        }
+a = input("Type what do you want to ask:")
+print(DATA[a])
+
+#PEÑAFIEL, R-JAY E. 12-CPE-01
+#DICTIONARY
